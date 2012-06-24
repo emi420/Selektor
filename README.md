a9a523f270e173d11fc843cf17b20d0bba901a75
Emilio Mariscal <emi420@gmail.com>
2012-06-24 12:10:20 -0300

    Initial commit.

---
+++ README.md
@@ -0,0 +1,27 @@
+Selektor, a simple JavaScript selector
+
+Selektor is licensed under the terms of either the MIT License or the GNU General Public License (GPL) Version 3.
+
+(c) 2012 Emilio Mariscal
+
+# Usage
+
+Selector:
+
+$("#myClass") - return an element 
+$(".myClass") - return an array of elements
+
+You can add your custom methods or properties, using the $.extend function:
+
+    $.extend({
+        cuack: function() {
+            var i = 0;
+            if(Object.prototype.toString.call( this ) === '[object NodeList]') {
+                for (i = this.length; i--;) {
+                    this[i].innerHTML += " - cuack!";
+                }
+            } else {
+                this.innerHTML += " - cuack!";                    
+            }
+        } 
+    });
---
+++ selektor.js
@@ -0,0 +1,73 @@
+/**
+ * Selektor, a simple JavaScript selector
+ * 
+ * https://github.com/emi420/selektor
+ * 
+ * Selektor is licensed under the terms of either the MIT License or the GNU General Public License (GPL) Version 3.
+ * 
+ * (c) 2012 Emilio Mariscal
+ * 
+ */
+
+(function(){
+    "use strict";
+    
+	var $ = function (query) {
+		return new Selektor(query);
+	};
+	
+	var Selektor = function (query) {
+		var query_type = typeof query,
+			el;
+
+        // Get element from query
+        if (query_type === "string") {
+
+            if (query.indexOf("#") > -1) {
+                query = query.replace("#", "");
+                el = document.getElementById(query);
+
+            } else if (query.indexOf(".") > -1) {
+                query = query.replace(".", "");
+                el = document.getElementsByClassName(query);
+
+            }
+            
+        } else if (query_type === "object") {
+            el = query;
+        }
+
+        // Instance properties
+        this.query = (function () {
+            return query;
+        }());
+        this.el = (function () {
+            return el;
+        }());
+        
+        $.extend($.prototype, el.constructor.prototype);
+        
+        return el;
+    }
+
+    // Extend function
+    $.extend = function (obj, target) {
+        var i;
+        if (target === undefined) {
+            target = $.prototype;
+        }
+        for (i in obj) {
+            if (obj.hasOwnProperty(i)) {
+                target[i] = obj[i];
+            }
+        }
+    };        
+       	
+    // Go public!
+    if (!window.$ || typeof ($) !== "function") {
+        window.$ = $;
+    }
+
+}());
+
+
